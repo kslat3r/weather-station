d4413cf604f3233178771a6ecf84fe579b60f277
Ed Kelly <me@edkelly.co.uk>
2020-07-01 13:47:55 +0100

    Refactor

--- start.py
+++ start.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-# -*- coding: utf-8 -*-
-
 import os
 from sense_hat import SenseHat
 from influxdb import InfluxDBClient
@@ -10,7 +8,7 @@ import time
 
 def get_cpu_temp():
     res = os.popen("vcgencmd measure_temp").readline()
-    return float(res.replace("temp=","").replace("'C\n",""))
+    return float(res.replace("temp=","").replace("'C\n", ""))
 
 def get_temperature(sense):
     sense_temp = sense.get_temperature()
@@ -19,31 +17,32 @@ def get_temperature(sense):
 
 try:
     sense = SenseHat()
+    sense.set_rotation(180)
+
     client = InfluxDBClient("localhost", 8086, "root", "root", "logger_db")
 
     while True:
         timestamp = datetime.datetime.utcnow().isoformat()
 
         temperature = get_temperature(sense)
-        pressure = sense.get_pressure()
         humidity = sense.get_humidity()
-        
-        
+        pressure = sense.get_pressure()
+
         datapoints = [{
             "measurement": "test1",
             "tags": {},
             "time": timestamp,
             "fields": {
                 "temperaturevalue": temperature,
-                "pressurevalue": pressure,
-                "humidityvalue": humidity
+                "humidityvalue": humidity,
+                "pressurevalue": pressure
             }
         }]
-        
+
         client.write_points(datapoints)
 
-        sense.show_message("OK")
-        
+        sense.show_message(str(round(temperature)) + "c " + str(round(humidity)) + "%RH " + str(round(pressure)) + "mBar", text_colour=[100, 100, 100])
+
         time.sleep(5)
 except KeyboardInterrupt:
-    print("Exit")
+	print("Exit")
