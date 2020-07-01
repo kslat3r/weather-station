a338e9dd2011418377684b52f7e06f2fe8added6
Ed Kelly <kslat3r@potato.local>
2020-07-01 11:46:29 +0100

    Refactor

--- start.py
+++ start.py
@@ -2,45 +2,36 @@
 
 # -*- coding: utf-8 -*-
 
-import argparse
 import time
 import datetime
-import sys
 from influxdb import InfluxDBClient
 from sense_hat import SenseHat
 
 sense = SenseHat()
-sampling_period = 5
-
-def get_data_points():
-    temperature = sense.get_temperature()
-    pressure = sense.get_pressure()
-    humidity = sense.get_humidity()
-    timestamp = datetime.datetime.utcnow().isoformat()
-    
-    datapoints = [{
-        "measurement": "test1",
-        "tags": {},
-        "time": timestamp,
-        "fields": {
-            "temperaturevalue": temperature,
-            "pressurevalue": pressure,
-            "humidityvalue": humidity
-        }
-    }]
-    
-    return datapoints
-
-# Initialize the Influxdb client
-
 client = InfluxDBClient("localhost", 8086, "root", "root", "logger_db")
 
 try:
      while True:
-        datapoints = get_data_points()
+        temperature = sense.get_temperature()
+        pressure = sense.get_pressure()
+        humidity = sense.get_humidity()
+        timestamp = datetime.datetime.utcnow().isoformat()
+        
+        datapoints = [{
+            "measurement": "test1",
+            "tags": {},
+            "time": timestamp,
+            "fields": {
+                "temperaturevalue": temperature,
+                "pressurevalue": pressure,
+                "humidityvalue": humidity
+            }
+        }]
+        
         client.write_points(datapoints)
 
         sense.show_message("OK")
+        
         time.sleep(5)
 except KeyboardInterrupt:
-    print ("Program stopped by keyboard interrupt [CTRL_C] by user. ")
+    print("Exit")
